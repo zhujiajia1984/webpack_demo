ec661bd035f7a0d1851819395f839d38651cf267
zhujiajia <772978409@qq.com>
2018-03-28 16:54:44 +0800

    新增css支持

---
+++ src/Components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+//
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="title">
+				App
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './Components/App/App';
 
 ReactDOM.render(
-    <h1>Hello, world222!</h1>,
+    <App></App>,
     document.getElementById('root')
 );
