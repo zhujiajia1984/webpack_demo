93aa943555dcd54923781788be00398fd7df27f7
zhujiajia <772978409@qq.com>
2018-03-28 16:43:01 +0800

    初始化

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>Hello, world222!</h1>,
+    document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    mode: 'development',
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        port: 18211,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+            }
+        }]
+    }
+}
